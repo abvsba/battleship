6507716b2695c239784712045e4ac9fe54574827
feiyu <f.liu@alumnos.upm.es>
2024-05-27 23:07:35 +0200

    Add: Rival table and rival ships #2

--- src/app/battleship-game/battleship-game.component.ts
+++ src/app/battleship-game/battleship-game.component.ts
@@ -11,16 +11,21 @@ import {Coordinate} from "../../shared/models/coordinate.model";
 })
 export class BattleshipGameComponent implements AfterViewInit{
   @ViewChildren('ship_self') listShipSelf!: QueryList<ElementRef>;
+  @ViewChildren('ship_rival') listShipRival!: QueryList<ElementRef>;
+  @ViewChildren('rival_cell') listRivalCell!: QueryList<ElementRef>;
 
   mapShipSelf = new Map();
+  mapShipRival = new Map();
 
   cellWidth = 40;
   selfBoard = new Board();
+  rivalBoard = new Board();
 
   selectedShip!: Ship;
   selectedDiv!: number;
 
   selfShipList: Ship[] = initShips();
+  rivalShipList: Ship[] = initShips();
 
   rowList: number[] = Array.from({length: 10}, (_, index) => index);
   colList: number[] = Array.from({length: 10}, (_, index) => index);
@@ -29,10 +34,14 @@ export class BattleshipGameComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
     this.initShips();
+    this.positionShipRandomly();
   }
 
   initShips(): void {
     for (let i = 0; i < this.selfShipList.length; i++) {
+
+      this.rivalShipList[i].isVisible = false;
+      this.mapShipRival.set(this.rivalShipList[i], this.listShipRival.get(i)!.nativeElement);
 
       this.mapShipSelf.set(this.selfShipList[i].type, this.listShipSelf.get(i)!.nativeElement);
     }
@@ -40,6 +49,56 @@ export class BattleshipGameComponent implements AfterViewInit{
 
   onDragStart(shipView: Ship) {
     this.selectedShip = shipView;
+  }
+
+  positionShipRandomly() {
+    let ship = 0
+    while (ship < this.rivalShipList.length) {
+      const randomNumber = Math.floor(Math.random() * 100);
+      let cellHTML = this.listRivalCell.get(randomNumber)?.nativeElement;
+      let row = cellHTML.getAttribute('data-x');
+      let col = cellHTML.getAttribute('data-y');
+
+      const head = new Cell(+row, +col);
+      this.selectedShip = this.rivalShipList[ship];
+      const shipHTML = this.mapShipRival.get(this.selectedShip);
+
+      if (Math.random() < 0.5) {
+        this.selectedShip.isHorizontal = false;
+        let dimension = new Coordinate(this.cellWidth, (this.cellWidth * this.selectedShip.length));
+        shipHTML.style.width = dimension.getLeftString();
+        shipHTML.style.height = dimension.getTopString();
+      }
+      if (this.setLeftRight(this.rivalBoard, head, shipHTML, cellHTML, this.notShipAround)) {
+        ship++;
+      }
+    }
+  }
+
+  notShipAround(app: any, head: Cell) {
+    let length = app.selectedShip.length;
+    let row = head.row;
+    let col = head.col;
+
+    return app.checkAdjacentCells(row, col) && (app.selectedShip.isHorizontal ?
+      !app.hasShipAtCell(row, col - 1) && !app.hasShipAtCell(row, col + length) :
+      !app.hasShipAtCell(row - 1, col) && !app.hasShipAtCell(row + length, col));
+  }
+
+  hasShipAtCell(row: number, col: number) {
+    return this.isValidCell(row, col) && this.rivalBoard.cell[row][col].hasShip();
+  }
+
+  checkAdjacentCells(row: number, col: number) {
+    let isHorizontal = this.selectedShip.isHorizontal;
+
+    for (let i = 0; i < this.selectedShip.length; i++) {
+      if (this.hasShipAtCell(row + (isHorizontal ? 1 : i), col + (isHorizontal? i : 1 )) ||
+        this.hasShipAtCell(row +  (isHorizontal ? -1 : i), col + (isHorizontal ? i : -1 ))) {
+        return false;
+      }
+    }
+    return true;
   }
 
   onDrop(event: DragEvent, row: number, col: number) {
@@ -51,14 +110,18 @@ export class BattleshipGameComponent implements AfterViewInit{
       let cellHTML = event.target as HTMLElement;
       const shipHTML = this.mapShipSelf.get(this.selectedShip.type);
 
-      this.setLeftRight(this.selfBoard, head, shipHTML, cellHTML);
+      this.setLeftRight(this.selfBoard, head, shipHTML, cellHTML, undefined);
     }
 
 
-  setLeftRight(board: Board, head: Cell, shipHTML: HTMLElement, cellHTML: HTMLElement) {
+  setLeftRight(board: Board, head: Cell, shipHTML: HTMLElement, cellHTML: HTMLElement, callback: Function | undefined) {
 
     let coordinate = this.shipIsInsideTable(head, cellHTML);
-    if (coordinate) {
+    if (coordinate && (!callback || callback(this, head))) {
+
+      if (!callback) {
+        this.selectedShip.oldHead = this.selectedShip.head;
+      }
 
       this.selectedShip.oldHead = this.selectedShip.head;
       shipHTML.style.left = coordinate.getLeftString();
