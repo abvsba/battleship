da9f1f4327c479672ef358bd71424375c90ca0ef
feiyu <f.liu@alumnos.upm.es>
2024-06-12 16:46:46 +0200

    Add: error dialog #13

--- src/app/service/userRest.service.ts
+++ src/app/service/userRest.service.ts
@@ -1,40 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {first, Observable, tap} from 'rxjs';
+import {catchError, EMPTY, first, Observable, tap, throwError} from 'rxjs';
 
 import {User} from "../../shared/models/user.model";
 import {JwtHelperService} from "@auth0/angular-jwt";
 import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserRestService {
+  static NOT_FOUND = 404;
+  static CONNECTION_REFUSE = 401;
+
   private BASE_URL = 'http://localhost:3000/user';
   private user : User | undefined;
 
-  constructor(private http: HttpClient, private router : Router) {}
+  private errorNotification = '';
+  private showErrors: boolean = true;
 
-  login(username: string, password: string): Observable<User> {
+  constructor(private http: HttpClient, private router : Router, private snackBar: MatSnackBar) {}
 
-    return this.http.post<User>(this.BASE_URL + '/login', {username, password}).pipe(
-      first(),
-      tap((jsonToken: any) => {
-        const jwtHelper = new JwtHelperService();
-        this.user = jsonToken;
-        this.user!.username = jwtHelper.decodeToken(jsonToken.token).username;
-        this.user!.email = jwtHelper.decodeToken(jsonToken.token).email;
-      })
+  login(username: string, password: string): Observable<any> {
+
+    return this.http.post<User>(this.BASE_URL + '/login', {username, password})
+      .pipe(
+        first(),
+        tap((jsonToken: any) => {
+          const jwtHelper = new JwtHelperService();
+          this.user = jsonToken;
+          this.user!.username = jwtHelper.decodeToken(jsonToken.token).username;
+          this.user!.email = jwtHelper.decodeToken(jsonToken.token).email;
+        }),
+        catchError(error => this.handleError(error))
     );
   }
 
   patchPassword(username : string, oldPassword: string, newPassword : string) {
     return this.http.patch(this.BASE_URL + '/' + username + '/password',
-      { oldPassword : oldPassword, newPassword : newPassword });
+      { oldPassword : oldPassword, newPassword : newPassword })
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  register(user : User): Observable<User> {
-    return this.http.post<User>(this.BASE_URL + '/signup', user);
+  register(user : User): Observable<any> {
+    return this.http.post<User>(this.BASE_URL + '/signup', user)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
   getUserDDBB(username: string) {
@@ -56,5 +67,37 @@ export class UserRestService {
 
   isAuthenticated(): boolean {
     return this.user != null && !(new JwtHelperService().isTokenExpired(this.user.token));
+  }
+
+  error(notification: string): UserRestService {
+    this.errorNotification = notification;
+    return this;
+  }
+
+  private showError(notification: string): void {
+    if(this.showErrors) {
+      if (this.errorNotification) {
+        this.snackBar.open(this.errorNotification, 'Error', {duration: 5000});
+        this.errorNotification = '';
+      } else {
+        this.snackBar.open(notification, 'Error', {duration: 5000});
+      }
+    }
+    this.showErrors = true;
+  }
+
+  private handleError(response: any): any {
+    if (response.status === UserRestService.NOT_FOUND || response.status === UserRestService.CONNECTION_REFUSE) {
+      this.showError('Username not exist or incorrect password');
+      return EMPTY;
+    } else {
+      try {
+        this.showError('(' + response.status + '): ' + response.error.message);
+        return throwError(() => response.error);
+      } catch (e) {
+        this.showError('Not response');
+        return throwError(() => response.error);
+      }
+    }
   }
 }
